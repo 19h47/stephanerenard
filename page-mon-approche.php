8fc273d413cf3a4314b00299a4bf7392184ad5ea
19h47 <levronjeremy@19h47.fr>
2018-02-10 11:30:00 +0100

    Mon approche page update

--- page-mon-approche.php
+++ page-mon-approche.php
@@ -1,5 +1,86 @@
 <?php
 
+/**
+ * page-mon-approche
+ */
+
+$fields = get_fields();
+
+$sections = array(
+	array(
+		'title'	=> $fields['title_first_pour-aller-plus-loin'],
+		'text'	=> $fields['text_first_pour-aller-plus-loin'],
+	),
+	array(
+		'title'	=> $fields['title_second_pour-aller-plus-loin'],
+		'text'	=> $fields['text_second_pour-aller-plus-loin'],
+	),
+	array(
+		'title'	=> $fields['title_third_pour-aller-plus-loin'],
+		'text'	=> $fields['text_third_pour-aller-plus-loin'],
+	),
+	array(
+		'title'	=> $fields['title_fourth_pour-aller-plus-loin'],
+		'text'	=> $fields['text_fourth_pour-aller-plus-loin'],
+	),
+);
+
+$output_sections = '';
+foreach ( $sections as $section ) {
+
+	if ( $section['title'] ) {
+		$output_sections .=	'<h3 class="h4 is-on-top color-blue-very-dark">';
+		$output_sections .= $section['title'];
+		$output_sections .= '</h3>';
+	}
+
+	if ( $section['text'] ) {
+		$output_sections .= '<div class="page-pour-aller-plus-loin__content js-column color-red-very-dark-grayish is-on-top">';
+		$output_sections .= $section['text'];
+		$output_sections .= '</div>';
+	}
+}
+
+$output_img_first = '';
+if ( ! empty( $fields['img_first'] ) ) {
+
+	$size = 'large';
+	$thumb = $fields['img_first']['sizes'][$size];
+
+	$output_img_first .= '<div class="col-xs-5 col-xs-offset-1 page-introduction__img--first" ';
+	$output_img_first .= "style=\"background-image: url( '{$thumb}' );\"></div>";
+
+}
+
+$output_img_second = '';
+if ( ! empty( $fields['img_second'] ) ) {
+
+	$size = 'large';
+	$thumb = $fields['img_second']['sizes'][$size];
+
+	$output_img_second .= '<div class="col-xs-10 col-xs-offset-2 page-introduction__img--second" ';
+	$output_img_second .= "style=\"background-image: url( '{$thumb}' );\"></div>";
+
+}
+
+$output_img_mon_parcours = '';
+if ( ! empty( $fields['img_mon-parcours'] ) ) {
+
+	$alt = $fields['img_mon-parcours']['alt'];
+	$size = 'large';
+	$large = $fields['img_mon-parcours']['sizes'][ $size ];
+	$width = $fields['img_mon-parcours']['sizes'][ $size . '-width' ];
+	$height = $fields['img_mon-parcours']['sizes'][ $size . '-height' ];
+
+	$output_img_mon_parcours .= '<img class="img-fluid" ';
+	$output_img_mon_parcours .= "src=\"{$large}\" ";
+	$output_img_mon_parcours .= "alt=\"{$alt}\" ";
+	$output_img_mon_parcours .= "width=\"{$width}\" ";
+	$output_img_mon_parcours .= "height=\"{$height}\"";
+	$output_img_mon_parcours .= '>';
+
+}
+
 get_header();
 
 while ( have_posts() ) : the_post(); ?>
@@ -7,46 +88,25 @@ while ( have_posts() ) : the_post(); ?>
 	<section class="site-section page-introduction row has-rounded-edge background-blue-very-dark">
 
 		<div class="site-section__inner col-xs-10 col-xs-offset-1 col-sm-5">
-			<h1 class="h1 color-white is-on-top">
-				<?php the_title(); ?>
-			</h1>
+
+			<?php the_title( '<h1 class="h1 color-white is-on-top">', '</h1>' ); ?>
+
 
 			<div class="page-introduction__paragraph is-on-top color-white">
-				<?php the_field( 'content_mon-parcours' ); ?>
-
+				<?php echo $fields['content_mon-parcours'] ?>
 			</div>
 		</div>
 
 		<div class="site-section__inner col-xs-6 hidden-xs js-animation-slide is-on-top">
 			<div class="row">
 
-				<?php $image_first = get_field( 'img_first' );
-				if ( ! empty( $image_first ) ) :
+				<?php
 
-					$size = 'large';
-					$thumb = $image_first['sizes'][ $size ]; ?>
+				echo
+					$output_img_first,
+					$output_img_second;
 
-					<div
-						class="col-xs-5 col-xs-offset-1 page-introduction__img--first"
-						style="background-image: url('<?php echo $thumb ?>');"
-					></div>
-
-				<?php endif;
-
-				$image_second = get_field( 'img_second' );
-				if ( ! empty( $image_second ) ) :
-
-					$size = 'large';
-					$thumb = $image_second['sizes'][ $size ];
-
-					?>
-					<div
-						class="col-xs-10 col-xs-offset-2 page-introduction__img--second"
-						style="background-image: url( '<?php echo $thumb ?>' );"
-					>
-					</div>
-
-				<?php endif; ?>
+				?>
 
 			</div>
 		</div>
@@ -54,29 +114,9 @@ while ( have_posts() ) : the_post(); ?>
 	</section>
 
 	<section class="site-section row">
+
 		<div class="site-secton__inner col-xs-10 col-xs-offset-1 col-sm-4 js-animation-slide is-on-top">
-
-			<?php
-
-			$image_mon_parcours = get_field( 'img_mon-parcours' );
-
-			if ( ! empty( $image_mon_parcours ) ) :
-
-				$alt = $image_mon_parcours['alt'];
-				$size = 'large';
-				$large = $image_mon_parcours['sizes'][ $size ];
-				$width = $image_mon_parcours['sizes'][ $size . '-width' ];
-				$height = $image_mon_parcours['sizes'][ $size . '-height' ]; ?>
-
-				<img
-					class="img-fluid" src="<?php echo $large ?>"
-					alt="<?php echo $alt ?>"
-					width="<?php echo $width ?>"
-					height="<?php echo $height ?>"
-				>
-
-			<?php endif ?>
-
+			<?php echo $output_img_mon_parcours ?>
 		</div>
 
 		<div class="site-section__inner page-mon-parcours--right col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0">
@@ -86,7 +126,7 @@ while ( have_posts() ) : the_post(); ?>
 			</h2>
 
 			<div class="page-mon-parcours--right__content color-red-very-dark-grayish is-on-top">
-				<?php the_field( 'texte_mon-parcours' ); ?>
+				<?php echo $fields['texte_mon-parcours'] ?>
 			</div>
 		</div>
 
@@ -95,58 +135,16 @@ while ( have_posts() ) : the_post(); ?>
 	<section class="site-section page-pour-aller-plus-loin row">
 
 		<div class="site-secton__inner col-xs-10 col-xs-offset-1">
+
 			<h2 class="h1 is-on-top color-cyan-dark-grayish">
 				<?php _e( 'Pour aller plus loin', 'stephanerenard' ) ?>
 			</h2>
+			<br>
 
-			<?php if ( get_field( 'title_first_pour-aller-plus-loin' ) ) : ?>
-				<h3 class="h4 is-on-top color-blue-very-dark">
-					<?php the_field( 'title_first_pour-aller-plus-loin' ); ?>
-				</h3>
-			<?php endif;
+			<?php echo $output_sections ?>
 
-			if( get_field( 'text_first_pour-aller-plus-loin' ) ) : ?>
-				<div class="page-pour-aller-plus-loin__content js-column color-red-very-dark-grayish is-on-top">
-					<?php the_field('text_first_pour-aller-plus-loin'); ?>
-				</div>
-			<?php endif; ?>
-
-			<?php if( get_field( 'title_second_pour-aller-plus-loin' ) ) : ?>
-				<h3 class="h4 is-on-top color-blue-very-dark">
-					<?php the_field('title_second_pour-aller-plus-loin'); ?>
-				</h3>
-			<?php endif; ?>
-
-			<?php if( get_field( 'text_second_pour-aller-plus-loin' ) ) : ?>
-				<div class="page-pour-aller-plus-loin__content js-column color-red-very-dark-grayish is-on-top">
-					<?php the_field('text_second_pour-aller-plus-loin'); ?>
-				</div>
-			<?php endif; ?>
-
-			<?php if( get_field( 'title_third_pour-aller-plus-loin' ) ) : ?>
-				<h3 class="h4 is-on-top color-blue-very-dark">
-					<?php the_field('title_third_pour-aller-plus-loin'); ?>
-				</h3>
-			<?php endif; ?>
-
-			<?php if( get_field( 'text_third_pour-aller-plus-loin' ) ) : ?>
-				<div class="page-pour-aller-plus-loin__content js-column color-red-very-dark-grayish is-on-top">
-					<?php the_field('text_third_pour-aller-plus-loin'); ?>
-				</div>
-			<?php endif; ?>
-
-			<?php if( get_field( 'title_fourth_pour-aller-plus-loin' ) ) : ?>
-				<h3 class="h4 is-on-top color-blue-very-dark">
-					<?php the_field( 'title_fourth_pour-aller-plus-loin' ); ?>
-				</h3>
-			<?php endif; ?>
-
-			<?php if( get_field( 'text_fourth_pour-aller-plus-loin' ) ) : ?>
-				<div class="page-pour-aller-plus-loin__content js-column color-red-very-dark-grayish is-on-top">
-					<?php the_field( 'text_fourth_pour-aller-plus-loin' ); ?>
-				</div>
-			<?php endif; ?>
 		</div>
+
 	</section>
 <?php
 
